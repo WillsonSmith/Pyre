d8b9bceb5ffa9b60acdc85df9c0ae076baa194f4
Willson Smith <me@willsonsmith.com>
2023-04-16 16:37:29 -0400

    don't log symlink error because it's most likely just one that already exists

--- packages/pyre/src/bin/pyre.ts
+++ packages/pyre/src/bin/pyre.ts
@@ -82,7 +82,7 @@ program
             });
           }
         } catch (e) {
-          console.error(e);
+          // console.error(e);
         }
       })
       .on('unlink', async (file) => {
